deee3c70bbea0a242bbecfdb940e80b49770b93d
Daniel Bornkessel <xx>
2021-07-09 12:20:18 +0200

    Trigger workflow on push as well

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ fetch(
     result = {}
     result.city = $("tbody tr:nth-of-type(28) th span").text();
     result.value = $("tbody tr:nth-of-type(28) td:nth-of-type(4)").text();
-    result.updated = new Date();
+    // result.updated = new Date(); // disable keep commits low
     fs.writeFile('resources/incidences.json', JSON.stringify(result), e => console.error(e));
   }
 );
