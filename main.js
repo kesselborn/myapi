8ad2abcfde2c20f192628c2c0d7d38254e5a7675
Daniel Bornkessel <xx>
2021-07-09 12:03:23 +0200

    First try

---
+++ main.js
@@ -0,0 +1,18 @@
+const cheerio = require("cheerio");
+const { fetch } = require("./scrapers/index.js");
+const fs = require('fs');
+
+
+fetch(
+  "https://www.juntadeandalucia.es/institutodeestadisticaycartografia/salud/static/resultadosProvincialesCovid.html?prov=29",
+  error => {
+    console.log(error);
+  },
+  html => {
+    const $ = cheerio.load(html);
+    result = {}
+    result.city = $("tbody tr:nth-of-type(28) th span").text();
+    result.value = $("tbody tr:nth-of-type(28) td:nth-of-type(4)").text();
+    fs.writeFile('resources/incidences.json', JSON.stringify(result), e => console.error(e));
+  }
+);
