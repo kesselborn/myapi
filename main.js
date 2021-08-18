0719a014ae018c9dbc1b61f3cd424df4c0e254ca
Daniel Bornkessel <git@bornkessel.com>
2021-08-18 13:42:05 +0200

    Get numbers for greece

--- main.js
+++ main.js
@@ -4,19 +4,37 @@ const fs = require('fs');
 
 
 fetch(
-  "https://www.juntadeandalucia.es/institutodeestadisticaycartografia/salud/static/resultadosProvincialesCovid.html?prov=29",
+  "https://corissia.com/de/coronavirus",
   error => {
     console.log(error);
   },
   html => {
     const $ = cheerio.load(html);
+    let element = $('div.col2-4')[1];
+    element = $('span.f-14.w-600', element)[1].parent;
     result = [{
-      city: $("tbody tr:nth-of-type(1)  th span").text(),
-      value: $("tbody tr:nth-of-type(1)  td:nth-of-type(4)").text()
-    },{
-      city: $("tbody tr:nth-of-type(28) th span").text(),
-      value: $("tbody tr:nth-of-type(28) td:nth-of-type(4)").text(),
+      region: "Kreta",
+      label: "14 Tage Inzidenz",
+      value: element.childNodes[2].data.trim(),
     }];
-    fs.writeFile('resources/incidences.json', JSON.stringify(result, null, 2), e => console.error(e));
+    fs.writeFile('resources/kreta.json', JSON.stringify(result, null, 2), e => console.error(e));
+  }
+);
+
+fetch(
+  "https://www.corona-in-zahlen.de/weltweit/griechenland/",
+  error => {
+    console.log(error);
+  },
+  html => {
+    const $ = cheerio.load(html);
+    let element = Array.from($('div.h-100'))[3];
+    element = $('p', element);
+    result = [{
+      region: "Griecheland",
+      label: "7 Tage Inzidenz",
+      value: element[0].childNodes[0].children[0].data.trim(),
+    }];
+    fs.writeFile('resources/griechenland.json', JSON.stringify(result, null, 2), e => console.error(e));
   }
 );
